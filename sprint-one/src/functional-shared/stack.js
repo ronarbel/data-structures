81c1b1595b26ec82eec9c5d1ddadbe6f86854da5
Ron Arbel <ronarbel1@gmail.com>
2018-11-07 13:29:17 -0800

    size test passing in stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  size: function() {
+    var size = 0;
+    for (var key in this.storage) {
+      size++;
+    }
+    return size;
+  }
+};

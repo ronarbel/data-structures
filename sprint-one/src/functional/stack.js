c5d10487e6c62fdb87976fd9dbdde6ccc2a83ab5
Ron Arbel <ronarbel1@gmail.com>
2018-11-07 12:35:12 -0800

    all Stack tests pass

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var nextStack = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[nextStack] = value;
+    nextStack++;
   };
 
   someInstance.pop = function() {
+    var result = storage[nextStack - 1];
+    delete storage[nextStack - 1];
+    nextStack--;
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;

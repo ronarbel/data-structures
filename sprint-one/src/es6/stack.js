46876536178a73ff425570e53307fcdab9c1f80f
Ron Arbel <ronarbel1@gmail.com>
2018-11-07 16:17:53 -0800

    es6 stack passing all tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.nextStack = 0;
   }
 
+  push(value) {
+    this.storage[this.nextStack] = value;
+    this.nextStack++;
+  }
+
+  pop() {
+    var result = this.storage[this.nextStack - 1];
+    delete this.storage[this.nextStack - 1];
+    this.nextStack--;
+    return result;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }

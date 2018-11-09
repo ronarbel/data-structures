1f40a85dd307620867add784241df5049a406766
Hack Reactor Student <student@hackreactor.com>
2018-11-08 20:42:09 -0800

    Complete all tree tests but nested children

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,9 +13,24 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // this.children.push(value);
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // base case: if current obj.value === target
+    // return true;
+  // else for each child in obj.children
+    // obj.children[el].contains(target)
+  // return false;
+  
+  if (this.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      return this.children[i].contains(target);
+    }
+  } return false;
 };
 
 

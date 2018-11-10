f594159f3e9f42bb5990f2939a6a867998bfcd69
Hack Reactor Student <student@hackreactor.com>
2018-11-09 16:22:51 -0800

    Complete all but one graph test

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -18,6 +18,13 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  // store this node's arcs
+  // iterate over node's arcs
+    // pass each node name (arcs[i]) into removeEdge(node, arcs[i])
+  var arcsToDeleteArr = this[node].arcs;
+  for (var i = 0; i < arcsToDeleteArr.length; i++) {
+    this.removeEdge(node, arcsToDeleteArr[i]);
+  }
   delete this[node];
 };
 
@@ -34,16 +41,28 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  this[fromNode].arcs.forEach(function(arc, i) {
-    if (arc === toNode) {
+  // this[fromNode].arcs.forEach(function(arc, i) {
+  //   if (arc === toNode) {
+  //     this[fromNode].arcs.splice(i, 1);
+  //   }
+  // });
+  // this[toNode].arcs.forEach(function(arc, i) {
+  //   if (arc === fromNode) {
+  //     this[toNode].arcs.splice(i, 1);
+  //   }
+  // });
+  var fromArcs = this[fromNode].arcs;
+  for (var i = 0; i < fromArcs.length; i++) {
+    if (fromArcs[i] === toNode) {
       this[fromNode].arcs.splice(i, 1);
     }
-  });
-  this[toNode].arcs.forEach(function(arc, i) {
-    if (arc === fromNode) {
+  }
+  var toArcs = this[toNode].arcs;
+  for (var i = 0; i < toArcs.length; i++) {
+    if (toArcs[i] === fromNode) {
       this[toNode].arcs.splice(i, 1);
     }
-  });
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
